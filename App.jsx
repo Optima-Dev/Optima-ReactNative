9c52e5358c487bbd8a8687ac52bccf35defadb50
Ahmed Hatem <ugs.1331@ci.suez.edu.eg>
2025-02-25 21:15:37 +0200

    justified the app

--- App.jsx
+++ App.jsx
@@ -1,29 +1,27 @@
 // importing navigation
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 // importing constants
-import Colors from './constants/Colors'; 
+import Colors from "./constants/Colors";
 
 // importing icons
-import { Ionicons } from '@expo/vector-icons';
+import { Ionicons } from "@expo/vector-icons";
 
 // importing react hooks
-import { useState } from 'react';
+import { useState } from "react";
 
-
-// importing screens 
-import Splash from './screens/Splash';
-import OnBoarding1 from './screens/OnBoarding1';
-import OnBoarding2 from './screens/OnBoarding2';
-import OnBoarding3 from './screens/OnBoarding3';
-import PrivacyTerms1 from './screens/PrivacyTerms1';
-import Start from './screens/Start';
-import Login from './screens/Login';
+// importing screens
+import Splash from "./screens/Splash";
+import OnBoarding1 from "./screens/OnBoarding1";
+import OnBoarding2 from "./screens/OnBoarding2";
+import OnBoarding3 from "./screens/OnBoarding3";
+import PrivacyTerms1 from "./screens/PrivacyTerms1";
+import Start from "./screens/Start";
+import Login from "./screens/Login";
 
 // importing components
-import BackButton from './components/UI/BackButton';
-
+import BackButton from "./components/UI/BackButton";
 
 // creating stack navigator
 const Stack = createNativeStackNavigator();
@@ -34,49 +32,61 @@ function StackNavigator() {
   const [step, setStep] = useState(1);
 
   return (
-    <Stack.Navigator 
+    <Stack.Navigator
       screenOptions={{
         headerShown: false,
-        contentStyle: {backgroundColor: Colors.MainColor}, // Correct usage
+        contentStyle: { backgroundColor: Colors.MainColor }, // Correct usage
         headerShadowVisible: false, // Remove shadow between content and header
-        headerStyle: { 
+        headerStyle: {
           shadowOpacity: 0, // Remove shadow on iOS
-          elevation: 0 // Remove shadow on Android
+          elevation: 0, // Remove shadow on Android
         },
-        headerStyle: { 
+        headerStyle: {
           backgroundColor: Colors.SeconderyColor,
         },
         headerTitle: "",
       }}>
-      <Stack.Screen name="Splash" component={Splash} options={{ 
-        headerShown: false, 
-        gestureEnabled: false // Disable going back to Splash
-      }} />
-      <Stack.Screen name="OnBoarding1">
-        {props => <OnBoarding1 {...props} step={step} setStep={setStep} />}
+      <Stack.Screen
+        name='Splash'
+        component={Splash}
+        options={{
+          headerShown: false,
+          gestureEnabled: false, // Disable going back to Splash
+        }}
+      />
+      <Stack.Screen name='OnBoarding1'>
+        {(props) => <OnBoarding1 {...props} step={step} setStep={setStep} />}
       </Stack.Screen>
-      <Stack.Screen name="OnBoarding2">
-        {props => <OnBoarding2 {...props} step={step} setStep={setStep} />}
+      <Stack.Screen name='OnBoarding2'>
+        {(props) => <OnBoarding2 {...props} step={step} setStep={setStep} />}
       </Stack.Screen>
-      <Stack.Screen name="OnBoarding3">
-        {props => <OnBoarding3 {...props} step={step} setStep={setStep} />}
+      <Stack.Screen name='OnBoarding3'>
+        {(props) => <OnBoarding3 {...props} step={step} setStep={setStep} />}
       </Stack.Screen>
-      <Stack.Screen name="Start" component={Start} 
-       options={{
-        headerShown: true,
-        headerLeft: () => <BackButton />,
-        
-      }}/>
-      <Stack.Screen name="PrivacyTerms1" component={PrivacyTerms1} options={{
-        headerShown: true,
-        headerLeft: () => <BackButton />,
-        
-      }} />
-      <Stack.Screen name="Login" component={Login} options={{
-        headerShown: true,
-        headerLeft: () => <BackButton />,
-        
-      }}/>
+      <Stack.Screen
+        name='Start'
+        component={Start}
+        options={{
+          headerShown: true,
+          headerLeft: () => <BackButton />,
+        }}
+      />
+      <Stack.Screen
+        name='PrivacyTerms1'
+        component={PrivacyTerms1}
+        options={{
+          headerShown: true,
+          headerLeft: () => <BackButton />,
+        }}
+      />
+      <Stack.Screen
+        name='Login'
+        component={Login}
+        options={{
+          headerShown: true,
+          headerLeft: () => <BackButton />,
+        }}
+      />
     </Stack.Navigator>
   );
 }
