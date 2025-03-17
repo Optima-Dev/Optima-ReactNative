cbb4ccb2fc515aa8b2067b1a2383cd894330957e
Abdelsalam Ebrahim <abdeslalamebrahim21@gmail.com>
2025-03-17 22:31:23 +0200

    add camera configuration in MyVision

--- screens/Seeker/MyVision.jsx
+++ screens/Seeker/MyVision.jsx
@@ -1,9 +1,65 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { useRef, useState } from 'react';
+import { Image, StyleSheet, View } from 'react-native';
+import { CameraView } from 'expo-camera';
+
+import PrimaryButton from '../../components/UI/PrimaryButton';
+import Colors from '../../constants/Colors';
 
 const MyVision = () => {
+  const cameraRef = useRef();
+  const [uri, setUri] = useState();
+
+  async function takePicture() {
+    if (cameraRef.current) {
+      const photo = await cameraRef.current.takePictureAsync();
+      setUri(photo.uri);
+    }
+  }
+
+  async function reTakePicture() {
+    setUri(null);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>MyVision</Text>
+
+      {
+        uri ? (
+          <Image
+            source={{ uri }}
+            style={styles.camera}
+          />
+        ) : (
+          <CameraView
+            style={styles.camera}
+            ref={cameraRef}
+          />
+        )
+      }
+
+      <View style={styles.buttonsContainer}>
+        <PrimaryButton
+          title="Take A Picture"
+          backgroundColor={Colors.MainColor}
+          textColor="white"
+          isLoading={false}
+          onPress={takePicture}
+          style={{ width: uri ? '48%' : '100%' }}
+        />
+
+        {
+          uri && (
+            <PrimaryButton
+              title="Repeat"
+              backgroundColor={Colors.green500}
+              textColor={Colors.MainColor}
+              isLoading={false}
+              onPress={reTakePicture}
+              style={{ width: '48%' }}
+            />
+          )
+        }
+      </View>
     </View>
   );
 }
@@ -14,7 +70,19 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 20,
-    justifyContent: 'center',
-    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.4)',
+  },
+  camera: {
+    flex: 1,
+    marginBottom: 20,
+    borderRadius: 20,
+    overflow: 'hidden',
+  },
+  focuedCamera: {
+    backgroundColor: 'white',
+  },
+  buttonsContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
   }
 });
